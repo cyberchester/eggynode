df3566b656aaae58c98da12f2b0dec7eb94a001b
cyberchester <cyberchester5@gmail.com>
2022-07-06 15:26:20 +0630

    ok

--- app.js
+++ app.js
@@ -469,7 +469,7 @@ app.post('/get_episode', (req, res)=>{
 
 
 //app.listen(process.env.PORT || 4000, () => console.log("server ready"))
-app.listen(4000, "192.168.99.32", () => console.log("server ready"))
+app.listen(4000, "192.81.216.81", () => console.log("server ready"))
 
 
 
